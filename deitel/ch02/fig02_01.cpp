9f20d9330cfe719a82401c89e73d8edf5a440c4d
Chuck Sylvester <chuck.sylvester@outlook.com>
2022-08-09 20:57:04 -0400

    Create Initial CPP source files
    Working through chapter 2

---
+++ deitel/ch02/fig02_01.cpp
@@ -0,0 +1,11 @@
+// Figure 2.1: fig02_01.cpp
+// Text-printing program.
+
+#include <iostream> // enables program to output data to screen
+
+// function main begins program execution
+int main() {
+    std::cout << "Welcome to C++!\n"; // display message
+
+    return 0; // indicate that program ended successfully
+}
---
+++ deitel/ch02/fig02_03.cpp
@@ -0,0 +1,8 @@
+// Figure 2.3: fig02_03.cpp
+// Printing a line of text with multiple statements.
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome ";
+    std::cout << "to C++!\n";
+}
