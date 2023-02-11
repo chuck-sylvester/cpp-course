e597a3dfb7a0404dcb3b8f61259347e73406c06e
Chuck Sylvester <chuck.sylvester@outlook.com>
2023-02-11 08:47:53 -0500

    working on c++ programming

--- deitel/10ed/ch03/BankAccount/source/Account.h
+++ deitel/10ed/ch03/BankAccount/source/Account.h
@@ -1,12 +1,14 @@
-// Fig. 3.4: Account.h
-// Account class that contains a name data member and member functions to set and get its value.
-// Update to class includes a constructor that initializes the account name.
+/*
+    Fig. 3.4: Account.h
+    Account class that contains a name data member and member functions to set and get its value.
+    Update to class includes a constructor that initializes the account name.
+*/
 #include <string>
 
 class Account {
 public:
-    // constructor initializes data member name wiht parameter accountName
-    explicit Account(std::string accountName) : name{accountName} {
+    // constructor initializes data member name with parameter accountName
+    explicit Account(std::string accountName) : name {accountName} {
         // empty body
     }
 
--- deitel/10ed/ch03/BankAccount/source/AccountTest.cpp
+++ deitel/10ed/ch03/BankAccount/source/AccountTest.cpp
@@ -1,6 +1,8 @@
-// Fig. 3.5: AccountTest.cpp
-// Creating and manipulating an Account object
-// Uses a constructor to initialize the name data member
+/*
+    Fig. 3.5: AccountTest.cpp
+    Creating and manipulating an Account object
+    Uses a constructor to initialize the name data member
+*/
 #include <iostream>
 #include <string>
 #include "Account.h"
@@ -9,14 +11,14 @@ using namespace std;
 
 int main() {
     // create two Account objects
-    Account myAccount1{"Jane Green"};
-    Account myAccount2{"John Blue"};
+    Account myAccount1 {"Jane Green"};
+    Account myAccount2 {"John Blue"};
 
     // show that the initial value of myAccount's name is the empty string
     cout << "Initial account 1 name is: " << myAccount1.getName() << endl;
     cout << "Initial account 2 name is: " << myAccount2.getName() << endl;
 
-    // prompt fro and read name
+    // prompt for and read name
     cout << "\nPlease enter the account name: ";
     string theName; 
     getline(cin, theName); // read a line of text
